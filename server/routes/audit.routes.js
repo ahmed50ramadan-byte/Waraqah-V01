499bedc569f703acdd1e9762f7fd6938ddb08fc0
ahmed50ramadan-byte <ahmed50ramadan@gmail.com>
2026-08-06 11:26:21 +0300

    Delete server directory

--- server/routes/audit.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protectStaff } = require('../middleware/auth');
-const { getAuditLogs, addAuditNote } = require('../controllers/audit.controller');
-
-router.get('/', protectStaff, getAuditLogs);
-router.post('/', protectStaff, addAuditNote);
-
-module.exports = router;
